f99987726677dfa34ddc951eeaf4ecd2c3e5fdee
VISHNU M K <futuralabs.mern@gmail.com>
2025-06-03 16:32:24 +0530

    Home page updated

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react"
+import { Button } from "react-bootstrap"
+import { Link } from "react-router"
 
 export const HomePage = () => {
-    const [counter, setCounter] = useState(0)
-    return <div>
-        <div>{counter}</div>
-        <button onClick={() => setCounter(counter + 1)}>Inc</button>
+    return <div className="d-flex flex-column align-items-center mt-3">
+        <h3>Navigations</h3>
+        <div className="flex gap-3 mt-3">
+            <Link to={"/todo"}><Button variant="success" size="sm">Todo List</Button></Link>
+        </div>
     </div>
 }
